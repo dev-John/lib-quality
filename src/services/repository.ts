7e621f30032cbfeea21cdc3148bf2af67825efc8
Jonatas <jonatasoliveira1234@gmail.com>
2021-04-17 18:05:08 -0300

    Comment abou the OCTOKIT LIB

--- src/services/repository.ts
+++ src/services/repository.ts
@@ -23,6 +23,7 @@ export async function getIssuesInfo({
       stdAge: round(dbRepo.stdAge, 2),
     };
   } else {
+    // I used the Github Octokit lib to simplify the access to their API https://developer.github.com/v3/
     const result = await octokit.rest.issues.listForRepo({
       owner,
       repo,
